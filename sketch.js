e4d1eb7a928eda129dd06fedc6b4dd2a8ccb9609
xSHIGUYx <shilohberscheid@gmail.com>
2019-05-23 15:30:19 -0600

    Optimized the animation image loading process

--- sketch.js
+++ sketch.js
@@ -4,8 +4,7 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let playerRun1, playerRun2, playerRun3, playerRun4, playerRun5, playerRun6, playerRun7, playerRun8;
-let playerMagic1, playerMagic2, playerMagic3, playerMagic4, playerMagic5, playerMagic6, playerMagic7, playerMagic8, playerMagic9;
+
 let runCycle = [];
 let magicCycle = [];
 let player1;
@@ -17,25 +16,17 @@ let enemyList = [];
 let playerDamageAlarm;
 
 function preload() {
-  playerRun1 = loadImage("assets/playerRun1.png");
-  playerRun2 = loadImage("assets/playerRun2.png");
-  playerRun3 = loadImage("assets/playerRun3.png");
-  playerRun4 = loadImage("assets/playerRun4.png");
-  playerRun5 = loadImage("assets/playerRun5.png");
-  playerRun6 = loadImage("assets/playerRun6.png");
-  playerRun7 = loadImage("assets/playerRun7.png");
-  playerRun8 = loadImage("assets/playerRun8.png");
-  runCycle = {currentImage: [playerRun1, playerRun2, playerRun3, playerRun4, playerRun5, playerRun6, playerRun7, playerRun8], imageNumber: 0};
-  playerMagic1 = loadImage("assets/playerMagic1.png");
-  playerMagic2 = loadImage("assets/playerMagic2.png");
-  playerMagic3 = loadImage("assets/playerMagic3.png");
-  playerMagic4 = loadImage("assets/playerMagic4.png");
-  playerMagic5 = loadImage("assets/playerMagic5.png");
-  playerMagic6 = loadImage("assets/playerMagic6.png");
-  playerMagic7 = loadImage("assets/playerMagic7.png");
-  playerMagic8 = loadImage("assets/playerMagic8.png");
-  playerMagic9 = loadImage("assets/playerMagic9.png");
-  magicCycle = {currentImage: [playerMagic1, playerMagic2, playerMagic3, playerMagic4, playerMagic5, playerMagic6, playerMagic7, playerMagic8, playerMagic9], imageNumber: 0};
+  runCycle = {currentImage: [], imageNumber: 0};
+  for (let i = 1; i < 9; i++) {
+    let playerRun = loadImage("assets/playerRun" + i + ".png");
+    runCycle.currentImage.push(playerRun);
+  }
+
+  magicCycle = {currentImage: [], imageNumber: 0}
+  for (let i = 1; i < 10; i++) {
+    let playerMagic = loadImage("assets/playerMagic" + i + ".png");
+    magicCycle.currentImage.push(playerMagic);
+  }
 }
 
 class Timer {
@@ -131,7 +122,7 @@ class Player {
       image(runSprite, this.x, this.y, this.width, this.width);
     }
     else {
-      image(playerRun1, this.x, this.y, this.width , this.width);
+      image(runCycle.currentImage[0], this.x, this.y, this.width , this.width);
     }
   }
 }
