c37fc68d5aecc484b3e8b308de5a504cd9c6c514
xSHIGUYx <47368990+xSHIGUYx@users.noreply.github.com>
2019-06-17 23:48:28 -0600

    Added an additional enemy. Added hitbox class. Fixed animation. Added states, and finalized project.

--- reflection.md
+++ reflection.md
@@ -2,3 +2,9 @@
 -Animation of sprites
 -Damage Collisions
 -Language Knowledge
+
+##Solutions
+-Since I had no way to just plug the sprites into some type of function, I made an animation loop for each situation. This was a decent way to go about things, however I would have prefered to make an "animate" function. I tried multiple times and could not get one to work with every scenario, so I gave up and continued on with hand making each type of scenario.
+-For damage collisions I originally had multiple issues with pixel perfecting everything. However, when I changed over to using the collide library everything became quite easy. I also stuck the hitboxes being created in their own class. This system means that I can add as many attacks to the main character and enemies as I want, however I ran out of time figuring it all out.
+-Javascript is not my favorite, or first choice for a programming language. It is not made for making games, and thus was very difficult to get around in. Even though I do feel that I probably could've made something greater with a little better time management, I still felt very awkward using it all nonetheless.
+-My favorite part was watching it all start to fall into place at the end. After getting the hitbox class set up and figuring out animation, it was simply a time crunch to fit it all together.
--- sketch.js
+++ sketch.js
@@ -6,12 +6,18 @@
 // - describe what you did to take this project "above and beyond"
 
 let FLOOR = 70;
-let runCycle, magicCycle, stabCycle;
-let rollerCycle;
+let runCycle, stabCycle, hitCycle;
+let rollerCycle, swordBoiWalkCycle, swordBoiAttackCycle;
+let rollerCycleTimer;
+let swordBoiWalkTimer;
+let swordBoiAttackTimer;
 let player;
 let blood;
 let bloodList = [];
 let enemyList = [];
+let hitBoxList = [];
+let points = 0;
+let state = "game";
 
 function preload() {
   runCycle = {currentImage: [], imageNumber: 0};
@@ -19,9 +25,9 @@ function preload() {
     runCycle.currentImage.push(loadImage("assets/playerRun/playerRun" + i + ".png"));
   }
 
-  magicCycle = {currentImage: [], imageNumber: 0};
-  for (let i = 1; i < 10; i++) {
-    magicCycle.currentImage.push(loadImage("assets/playerMagic/playerMagic" + i + ".png"));
+  hitCycle = {currentImage: [], imageNumber: 0};
+  for (let i = 1; i < 9; i++) {
+    hitCycle.currentImage.push(loadImage("assets/playerHit/hit" + i + ".png"));
   }
 
   rollerCycle = {currentImage: [], imageNumber: 0};
@@ -32,6 +38,16 @@ function preload() {
   stabCycle = {currentImage: [], imageNumber: 0};
   for (let i = 1; i < 13; i++) {
     stabCycle.currentImage.push(loadImage("assets/playerStab/playerStab" + i + ".png"));
+  }
+
+  swordBoiWalkCycle = {currentImage: [], imageNumber: 0};
+  for (let i = 1; i < 5; i++) {
+    swordBoiWalkCycle.currentImage.push(loadImage("assets/swordBoi/walking/swordBoi" + i + ".png"));
+  }
+
+  swordBoiAttackCycle = {currentImage: [], imageNumber: 0};
+  for (let i = 1; i < 11; i++) {
+    swordBoiAttackCycle.currentImage.push(loadImage("assets/swordBoi/attacking/attack" + i + ".png"));
   }
 
   blood = loadImage("assets/blood.png");
@@ -88,26 +104,17 @@ class Player {
     this.move_right = false;
     this.move_left = false;
     this.isAttacking = false;
-    this.damageAlarm = new Timer(0);
     this.attackTimer = new Timer(0);
     this.runTimer = new Timer(0);
+    this.hitTimer = new Timer(0);
     this.damageDealt = 5;
     this.tempHealth = this.health;
-  }
-
-  damageCheck() {
-    for (let i = 0; i < enemyList.length; i++) {
-      if (enemyList[i].x < this.x + this.width - 35 && enemyList[i].x + enemyList[i].width > this.x + 35 
-        && this.damageAlarm.isDone() && magicCycle.imageNumber < 4 && enemyList[i].damageAlarm.isDone()) {
-        this.damageAlarm = new Timer(600);
-        this.health -= enemyList[i].damageDealt;
-      }
-    }
-    player.health = constrain(player.health, 0, player.health);
+    this.attackCycle = stabCycle;
+    this.isHit = false;
   }
 
   move() {
-    if (!this.isAttacking) {
+    if (!this.isAttacking && !this.isHit) {
       if (this.move_right && this.x + this.width <= width) {
         this.x += this.speed;
       }
@@ -118,6 +125,7 @@ class Player {
   }
 
   drawHealth() {
+    player.health = constrain(player.health, 0, player.health);
     if (this.tempHealth !== this.health) {
       this.tempHealth -= 0.5;
     }
@@ -135,38 +143,63 @@ class Player {
     rect(20, 20, this.tempHealth * 5, 50);
   }
 
-  draw() {
-    //Alt Attack animation
-    if (this.isAttacking) {
-      // fill(255, 0 , 0);
-      // rect(this.x + this.width / 2, this.y + this.width / 3, this.width * 1.5, this.width / 2);
-      if (this.attackTimer.isDone()) {
-        if (stabCycle.imageNumber + 1 < stabCycle.currentImage.length) {
-          stabCycle.imageNumber++;
-        } 
-        else {
-          stabCycle.imageNumber = 0;
-          this.isAttacking = false;
-        }
-        this.attackTimer = new Timer(80);
-      }
-      image(stabCycle.currentImage[stabCycle.imageNumber], this.x + 20, this.y - 128, this.width * 2, this.width * 2);
+  hitBoxCreate() {
+    if (this.isAttacking && this.attackCycle === stabCycle && this.attackCycle.imageNumber >= 9) {
+      hitBoxList.push(new Hitbox(this.x + this.width, this.y, this.width, "player"));
     }
-    ///Movement animation
-    else if (this.move_right || this.move_left) {
-      if (this.runTimer.isDone()) {
-        if (runCycle.imageNumber + 1 < runCycle.currentImage.length) {
-          runCycle.imageNumber++;
-        } 
-        else {
-          runCycle.imageNumber = 0;
+    else if (this.isAttacking && this.attackCycle === stabCycle 
+      && this.attackCycle.imageNumber < 5 && this.attackCycle.imageNumber > 2){
+      hitBoxList.push(new Hitbox(this.x + this.width / 2, this.y, this.width, "player"));
+    }
+  }
+
+  draw() {
+    if (!this.isHit) {
+      //Attack animation
+      if (this.isAttacking) {
+        if (this.attackTimer.isDone()) {
+          if (this.attackCycle.imageNumber + 1 < this.attackCycle.currentImage.length) {
+            this.attackCycle.imageNumber++;
+          } 
+          else {
+            this.attackCycle.imageNumber = 0;
+            this.isAttacking = false;
+          }
+          this.attackTimer = new Timer(80);
         }
-        this.runTimer = new Timer(80);
+        image(this.attackCycle.currentImage[this.attackCycle.imageNumber], this.x + 20, this.y - 128, this.width * 2, this.width * 2);
       }
-      image(runCycle.currentImage[runCycle.imageNumber], this.x, this.y, this.width, this.width);
+      ///Movement animation
+      else if (this.move_right || this.move_left) {
+        if (this.runTimer.isDone()) {
+          if (runCycle.imageNumber + 1 < runCycle.currentImage.length) {
+            runCycle.imageNumber++;
+          } 
+          else {
+            runCycle.imageNumber = 0;
+          }
+          this.runTimer = new Timer(80);
+        }
+        image(runCycle.currentImage[runCycle.imageNumber], this.x, this.y, this.width, this.width);
+      }
+      else {
+        image(runCycle.currentImage[0], this.x, this.y, this.width , this.width);
+      }
     }
     else {
-      image(runCycle.currentImage[0], this.x, this.y, this.width , this.width);
+      //Attack animation
+      if (this.hitTimer.isDone()) {
+        if (hitCycle.imageNumber + 1 < hitCycle.currentImage.length) {
+          hitCycle.imageNumber++;
+        }
+        else {
+          hitCycle.imageNumber = 0;
+            this.isHit = false;
+          }
+          this.hitTimer = new Timer(80);
+        }
+      image(hitCycle.currentImage[hitCycle.imageNumber], this.x, this.y, this.width, this.width);
+      this.x -= 3;
     }
   }
 }
@@ -174,45 +207,119 @@ class Player {
 class Enemy {
   constructor(type) {
     this.type = type;
-    if (type === "walker") {
+    if (type === "roller") {
       this.width = 120;
-      this.x = width - this.width;
+      this.x = width;
+      this.startHurtX = this.x;
+      this.endHurtX = this.width;
       this.y = height - FLOOR - this.width;
-      this.speed = -3;
+      this.speed = random(-4, -3);
       this.health = 10;
+      this.maxHealth = this.health;
+      this.damageDealt = 10;
+      this.damageAlarm = new Timer(0);
+      this.cycle = rollerCycle;
+      this.isAttacking = false;
+    }
+
+    if (type === "swordBoi") {
+      this.width = 128;
+      this.x = width;
+      this.startHurtX = this.x + 50;
+      this.endHurtX = this.width - 50;
+      this.y = height - FLOOR - this.width;
+      this.speed = random(-2, -1);
+      this.health = 20;
       this.maxHealth = this.health;
       this.damageDealt = 25;
       this.damageAlarm = new Timer(0);
-      this.timer = new Timer(0);
-      this.cycle = rollerCycle;
+      this.cycle = swordBoiWalkCycle;
+      this.isAttacking = false;
     }
   }
 
   move() {
-    if (this.damageAlarm.isDone()) {
+    if (this.type === "swordBoi") {
+      this.startHurtX = this.x + 50;
+      this.endHurtX = this.width - 50;
+      if (collideRectRect(this.x + 10, this.y, this.width, this.width, 
+        player.x + 40, player.y, player.width - 80, player.width) && this.damageAlarm.isDone()) {
+          this.cycle = swordBoiAttackCycle;
+          this.isAttacking = true;
+        }
+    }
+    else if (this.type === "roller") {
+      this.startHurtX = this.x;
+      this.endHurtX = this.width;
+      hitBoxList.push(new Hitbox(this.x, this.y, this.width, this));
+    }
+    if (this.damageAlarm.isDone() && !this.isAttacking) {
       this.x += this.speed;
     }
-    else {
+    else if (!this.damageAlarm.isDone()){
       this.x += 5;
     }
   }
 
+  createHitBox() {
+    if (this.type === "swordBoi") {
+      if (this.isAttacking && this.cycle.imageNumber > 5) {
+        hitBoxList.push(new Hitbox(this.x - this.width/2, this.y, this.width, this));
+      }
+    }
+  }
+
   draw() {
-    if (this.timer.isDone()) {
-      if (this.cycle.imageNumber + 1 < this.cycle.currentImage.length) {
-        this.cycle.imageNumber++;
+    if (this.health <= 0) {
+      for (let i = 0; i < random(5, 10); i++) {
+        bloodList.push(new Blood(this));
+      }
+    }
+    if (this.type === "roller") {
+      if (rollerCycleTimer.isDone()) {
+        if (this.cycle.imageNumber + 1 < this.cycle.currentImage.length) {
+          this.cycle.imageNumber++;
+        }
+        else {
+          this.cycle.imageNumber = 0;
+        }
+        rollerCycleTimer = new Timer(90);
+      }
+      image(this.cycle.currentImage[this.cycle.imageNumber], this.x, this.y, this.width, this.width);
+    }
+    else if (this.type === "swordBoi"){
+      if (this.isAttacking) {
+        if (swordBoiAttackTimer.isDone()) {
+          if (this.cycle.imageNumber + 1 < this.cycle.currentImage.length) {
+            this.cycle.imageNumber++;
+          } 
+          else {
+            this.cycle.imageNumber = 0;
+            this.cycle = swordBoiWalkCycle;
+            this.isAttacking = false;
+          }
+          swordBoiAttackTimer = new Timer(80);
+        }
+        image(this.cycle.currentImage[this.cycle.imageNumber], this.x, this.y, this.width, this.width);
       }
       else {
-        this.cycle.imageNumber = 0;
-      }
-      this.timer = new Timer(90);
+        if (swordBoiWalkTimer.isDone()) {
+          if (this.cycle.imageNumber + 1 < this.cycle.currentImage.length) {
+            this.cycle.imageNumber++;
+          }
+          else {
+            this.cycle.imageNumber = 0;
+          }
+          swordBoiWalkTimer = new Timer(100);
+        }
+        image(this.cycle.currentImage[this.cycle.imageNumber], this.x, this.y, this.width, this.width);
+      } 
     }
-    image(this.cycle.currentImage[this.cycle.imageNumber], this.x, this.y, this.width, this.width);
   }
 
   drawHealth() {
     fill(0);
-    rect(this.x, this.y - 20, this.width, 10);
+    rect(this.x, this.y - 20, this.width * this.maxHealth / 10, 10);
     if (this.health >= this.maxHealth * 0.6) {
       fill(0, 215, 0);
     }
@@ -222,52 +329,126 @@ class Enemy {
     else{
       fill(215, 0, 0);
     }
-    rect(this.x, this.y - 20, this.health * this.width / 10, 10);
+    rect(this.x, this.y - 20, this.width * this.health / 10, 10);
+  }
+}
+
+class Hitbox {
+  constructor(x, y, width, creator) {
+    this.creator = creator;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.timeToDie = false;
   }
 
-  damageCheck() {
-    if (this.x <= player.x + player.width * 1.5 && this.x + this.width >= player.x + player.width / 2 && this.damageAlarm.isDone() 
-    && player.isAttacking && stabCycle.imageNumber >= 9) {
-      this.damageAlarm = new Timer(400);
-      this.health -= player.damageDealt;
-      for (let i = 0; i < random(5, 10); i++) {
-        bloodList.push(new Blood(this));
+  checkForHit() {
+    // fill("black");
+    // rect(this.x, this.y, this.width, this.width);
+    if (this.creator === "player") {
+      for(let i = 0; i < enemyList.length; i++) {
+        if (collideRectRect(this.x, this.y, this.width, this.width, 
+          enemyList[i].startHurtX, enemyList[i].y, enemyList[i].endHurtX, enemyList[i].width) && enemyList[i].damageAlarm.isDone()) {
+          enemyList[i].damageAlarm = new Timer(400);
+          enemyList[i].health -= player.damageDealt;
+          if (enemyList[i].type === "swordBoi") {
+            enemyList[i].cycle.imageNumber = 0;
+            enemyList[i].cycle = swordBoiWalkCycle;
+            enemyList[i].isAttacking = false;
+          }
+        }
       }
+      this.timeToDie = true;
+    }
+    else {
+      if (collideRectRect(this.x, this.y, this.width, this.width,
+        player.x + 40, player.y, player.width - 80, player.width) && !player.isHit) {
+          if (this.creator.type === "roller") {
+            this.creator.speed = -12;
+          }
+          player.isHit = true;
+          player.health -= this.creator.damageDealt;
+      }
+      this.timeToDie = true;
     }
   }
 }
 
 function setup() {
   createCanvas(1600, 789);
+  textAlign(CENTER);
+  rollerCycleTimer = new Timer(0);
+  swordBoiWalkTimer = new Timer(0);
+  swordBoiAttackTimer = new Timer(0);
   player = new Player(25, 4);
-  window.setInterval(enemyCreate, 5000);
+  window.setInterval(enemyCreate, 2500);
 }
 
 function enemyCreate() {
-  enemyList.push(new Enemy("walker"));
+  enemyList.push(random([new Enemy("roller"), new Enemy("swordBoi")]));
 }
 
 function draw() {
-  drawBackground();
-  playerFunctions();
-  enemyFunctions();
-  for (let i = 0; i < bloodList.length; i++) {
-    bloodList[i].move();
-    bloodList[i].draw();
+  if (state === "game") {
+    drawBackground();
+    playerFunctions();
+    enemyFunctions();
+    bloodFunctions();
+    hitBoxFunctions();
+    displayPoints();
+  }
+  else {
+    drawBackground()
+    textSize(280);
+    fill(0);
+    text("Game Over", width/2, height/2);
+    textSize(100);
+    fill(0);
+    text("Press 'R' to Restart!", width/2, height/2 + 200);
+  }
+}
+
+function displayPoints() {
+  textSize(100);
+  fill(0);
+  text("Points: " + points, width/2, 0 + 100);
+}
+
+function hitBoxFunctions() {
+  for (let i = 0; i < hitBoxList.length; i++) {
+    hitBoxList[i].checkForHit();
+    if (hitBoxList[i].timeToDie) {
+      hitBoxList.splice(i, 1);
+    }
   }
 }
 
 function drawBackground() {
-  background(220);
+  background("blue");
   ///FLOOR
-  fill(0);
+  fill("orange");
   rect(0, height - FLOOR, width, 75);
 }
 
+function bloodFunctions() {
+  for (let i = 0; i < bloodList.length; i++) {
+    bloodList[i].move();
+    bloodList[i].draw();
+  }
+  for (let i = bloodList.length - 1; i >= 0; i--) {
+    if (bloodList[i].alpha > 255) {
+      bloodList.splice(i, 1);
+    }
+  }
+}
+
 function playerFunctions() {
+  if (player.tempHealth <= 0) {
+    state = "gameOver"
+  }
   player.draw();
   player.move();
-  player.damageCheck();
+  player.hitBoxCreate();
   player.drawHealth();
 }
 
@@ -276,11 +457,12 @@ function enemyFunctions() {
     enemyList[i].move();
     enemyList[i].draw();
     enemyList[i].drawHealth();
-    enemyList[i].damageCheck();
+    enemyList[i].createHitBox();
   }
   for (let i = enemyList.length - 1; i >= 0; i--) {
     if (enemyList[i].health <= 0 || enemyList[i].x + enemyList[i].width <= 0) {
       enemyList.splice(i, 1);
+      points += 50;
     }
   }
 }
@@ -294,6 +476,14 @@ function keyPressed() {
   }
   if (key === "a") {
     player.isAttacking = true;
+  }
+  if (key === "r" && state !== "game") {
+    player.health = player.maxHealth;
+    player.tempHealth = player.maxHealth;
+    points = 0;
+    enemyList = [];
+    player.x = 25;
+    state = "game";
   }
 }
 
